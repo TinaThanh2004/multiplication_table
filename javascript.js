eb7cdf49eb00bdf77c3804ee7812b98b0970dcc7
tinanguyen <nguy4938@umn.edu>
2025-10-01 15:11:48 -0500

    it works, pink button version

--- javascript.js
+++ javascript.js
@@ -1,50 +1,128 @@
-/* alert('hello world!') */
+// /* alert('hello world!') */
+//
+// /*loop examples*/
+// console.log('Here\'s a for loop showing 1...10:\n!')
+// for (let i = 0; i < 10; i++) {
+//     console.log(`${i}`)
+// }
+//
+// console.log('Here\'s a do-while for loop showing 1...10:\n!')
+// let i = 0
+// do {
+//     console.log(i)
+//     i++
+// } while (i < 10)
+//
+// console.log('Here\'s awhile for loop showing 1...10:\n!')
+// let j = 0
+// while (j < 10) {
+//     console.log(`${j}`)
+//     j++
+// }
+//
+// console.log('Here\'s a foreach loop iterating over an array showing 1...10:\n!')
+// let nums = [...Array(10).keys()]
+// nums.forEach(num => {
+//     console.log(`${num}`)
+// })
+//
+//
+//
+//
+// console.log('Here\'s a multiplication table method rendered in the console with a far loop:\n!')
+//
+// let timesTable = (to) => {
+//     if (Number.isInteger(to)) {
+//         // rows iterator
+//         for (let i = 1; i <= to; i++) {
+//             let row = ""
+//             //columns iterator
+//             for (let j = 1; j <= to; j++) {
+//                 row += `${ i * j }\t`
+//             }
+//             console.log(row)
+//         }
+//     } else {
+//         console.log(`Sorry, ${to} isn't a number!`)
+//     }
+// }
+//
+// timesTable(12)
+//
+// timesTable(20)
+//
+// timesTable('x')
+//
+//
+//
+//
+//
 
-/*loop examples*/
-console.log('Here\'s a for loop showing 1...10:\n!')
-for (let i = 0; i < 10; i++) {
-    console.log(`${i}`)
+
+
+
+//
+function getRowsDesired() {
+    // console.log(Number(document.querySelector('.rows input#rows').value))
+    // console.log(Number(document.querySelectorAll('.rows input')[0].value))
+    console.log(Number(document.getElementById('rows').value))
+    return Number(document.getElementById('rows').value);
 }
 
-console.log('Here\'s a do-while for loop showing 1...10:\n!')
-let i = 0
-do {
-    console.log(i)
-    i++
-} while (i < 10)
-
-console.log('Here\'s awhile for loop showing 1...10:\n!')
-let j = 0
-while (j < 10) {
-    console.log(`${j}`)
-    j++
+function getColumnsDesired() {
+    console.log(Number(document.getElementById('cols').value))
+    return Number(document.getElementById('cols').value)
 }
 
-console.log('Here\'s a foreach loop iterating over an array showing 1...10:\n!')
-let nums = [...Array(10).keys()]
-nums.forEach(num => {
-    console.log(`${num}`)
-})
 
+function createWithHtmlTable(rows, cols) {
+    let string
+    if (rows && cols) {
+        string = "<table>\n"
 
+        for (let row = 1; row <= rows; row++) {
+            string += `<tr>\n`
 
-
-console.log('Here\'s a multiplication table method rendered in the console with a far loop:\n!')
-
-let timesTable = (to) => {
-    if (Number.isInteger(to)) {
-        // rows iterator
-        for (let i = 1; i <= to; i++) {
-            let row = ""
-            //columns iterator
-            for (let j = 1; j <= to; j++) {
-                row += `${ i * j }\t`
+            for (let col = 1; col <= cols; col++) {
+                string += "<td>"
+                string += row * col
+                string += "<td>\n"
             }
-            console.log(row)
+            string += "</tr>\n"
         }
-    } else {
-        console.log(`Sorry, ${to} isn't a number!`)
-    }
-}
 
-timesTable(4)
+        string += "</table>\n"
+    } else {
+        string = 'Provide some inputs'
+    }
+    document.getElementById("output").innerHTML = string;
+
+}
+// createWithHtmlTable ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

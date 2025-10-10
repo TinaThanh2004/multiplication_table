845b05a9729ca9854e4cd2bf0dda1d1f15123c27
tinanguyen <nguy4938@umn.edu>
2025-10-10 18:57:49 -0500

    Fix with flexbox

--- javascript.js
+++ javascript.js
@@ -1,58 +1,83 @@
-function createWithHtmlTable(rows, cols, highlight) {
-    let string
-    if (rows && cols) { // if rows/columns are provided
-        string = "<table>\n" // build table
+function createWithFlexbox(rows, cols, highlight) {
+    let string; // same variable name as before
+
+    if (rows && cols) {
+        string = "<div class='flex-table'>\n"; // main container
 
         for (let row = 1; row <= rows; row++) {
             if (highlight && highlight === row) {
-                string += "<tr class='highlight'>\n"
+                string += "<div class='row highlight'>\n";
             } else {
-                string += "<tr>\n" // start row
+                string += "<div class='row'>\n";
             }
+
             for (let col = 1; col <= cols; col++) {
                 if (highlight && highlight === col) {
-                    string += "<td class='highlight'>\n"
+                    string += "<div class='cell highlight'>\n";
                 } else {
-                    string += "<td>" // start cell
+                    string += "<div class='cell'>\n";
                 }
 
-                string += row * col // content
-                string += "</td>\n" // end cell
+                string += row * col; // same logic as before
+                string += "</div>\n"; // close .cell
             }
-            string += "</tr>\n" // end row
+
+            string += "</div>\n"; // close .row
         }
-        string += "</table>\n" // end table
+
+        string += "</div>\n"; // close .flex-table
     } else {
-        string = 'Provide some inputs'
+        string = "Provide some inputs";
     }
 
-    document.getElementById('output').innerHTML = string;
+    document.getElementById("output").innerHTML = string;
 }
 
-// this method can be used to replace the 3 duplicates above = less code
-function getInputValueAsNumber(inputId) {
-    if (document.getElementById(inputId).value !== null) {
-        return Number(document.getElementById(inputId).value)
-    } else {
-        console.log(`Sorry, value isn\'t present for this ID: `, inputId)
-    }
 
-    console.log(`Sorry, couldn\'t find this ID: `, inputId)
-}
 
-// getRowsDesired() is equivalent to getInputAsNumber("rows")
-// getColsDesired() is equivalent to getInputAsNumber("cols")
-// getHighlightDesired() is equivalent to getInputAsNumber("highlight")
+// function createWithHtmlTable(rows, cols, highlight) {
+//     let string
+//     if (rows && cols) {
+//         string = "<table>\n"
 //
-console.log(`should get value of rows input: `, getInputValueAsNumber('rows'));
-console.log(`should get value of cols input:`, getInputValueAsNumber('cols'));
-console.log(`should get value of highlight input:`, getInputValueAsNumber('highlight'));
-
-
-
-
-
-
-
-
+//         for (let row = 1; row <= rows; row++) {
+//             if (highlight && highlight === row) {
+//                 string += "<tr class='highlight'>\n"
+//             } else {
+//                 string += "<tr>\n"
+//             }
+//             for (let col = 1; col <= cols; col++) {
+//                 if (highlight && highlight === col) {
+//                     string += "<td class='highlight'>\n"
+//                 } else {
+//                     string += "<td>"
+//                 }
+//
+//                 string += row * col
+//                 string += "</td>\n"
+//             }
+//             string += "</tr>\n"
+//         }
+//         string += "</table>\n"
+//     } else {
+//         string = 'Provide some inputs'
+//     }
+//
+//     document.getElementById('output').innerHTML = string;
+// }
+//
+// function getInputValueAsNumber(inputId) {
+//     if (document.getElementById(inputId).value !== null) {
+//         return Number(document.getElementById(inputId).value)
+//     } else {
+//         console.log(`Sorry, value isn\'t present for this ID: `, inputId)
+//     }
+//
+//     console.log(`Sorry, couldn\'t find this ID: `, inputId)
+// }
+//
+// console.log(`should get value of rows input: `, getInputValueAsNumber('rows'));
+// console.log(`should get value of cols input:`, getInputValueAsNumber('cols'));
+// console.log(`should get value of highlight input:`, getInputValueAsNumber('highlight'));
+//
 
